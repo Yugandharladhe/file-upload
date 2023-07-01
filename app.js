7970d483972778a091f9c9bdd9628c8647f3d9e6
Yugandharladhe <yugandharladhe74@gmail.com>
2023-07-01 17:14:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const app=express()
+const multer=require("multer")
+const PORT=process.env.PORT || 3000
+
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        return cb(null,"./uploads");
+    },
+    filename:function(req,file,cb){
+        return cb(null,`${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload=multer({storage:storage})
+
+app.post("/upload",upload.single("Pimage"),(req,res)=>{
+    console.log(req.body)
+    console.log(req.file)
+    return res.send(req.file)
+})
+app.listen(PORT,()=>{
+    console.log("app is listening")
+})
